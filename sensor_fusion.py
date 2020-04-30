7bfa921d0783e7ab75ffc5d3380c825b727dfd20
alexajan <ajan@mit.edu>
2020-04-29 23:39:18 -0700

    refactor

--- sensor_fusion.py
+++ sensor_fusion.py
@@ -2,6 +2,43 @@ import csv
 import sys
 import matplotlib.pyplot as plt
 
+
+def plot_data(accel, mag, gyro, filename):
+    # yellow = x
+    # green = y
+    # blue = z
+
+    x = [i[0] for i in accel]
+    y = [j[1] for j in accel]
+    z = [k[2] for k in accel]
+
+    plt.plot(x)
+    plt.plot(y)
+    plt.plot(z)
+    plt.savefig(filename + "_accel.png")
+    plt.show()
+
+    x_m = [i[0] for i in mag]
+    y_m = [j[1] for j in mag]
+    z_m = [k[2] for k in mag]
+
+    plt.plot(x_m)
+    plt.plot(y_m)
+    plt.plot(z_m)
+    plt.savefig(filename + "_mag.png")
+    plt.show()
+
+    x_g = [i[0] for i in gyro]
+    y_g = [j[1] for j in gyro]
+    z_g = [k[2] for k in gyro]
+
+    plt.plot(x_g)
+    plt.plot(y_g)
+    plt.plot(z_g)
+    plt.savefig(filename + "_gyro.png")
+    plt.show()
+
+
 def parse_data():
     filename = sys.argv[1]
 
@@ -35,41 +72,12 @@ def parse_data():
                 temp_i = row[0].find(":")
                 temp.append(float(row[0][temp_i+3:-1]))
 
-        # yellow = x
-        # green = y
-        # blue = z
+        return rssi, accel, mag, gyro
 
 
-        x = [i[0] for i in accel]
-        y = [j[1] for j in accel]
-        z = [k[2] for k in accel]
-
-        plt.plot(x)
-        plt.plot(y)
-        plt.plot(z)
-        plt.savefig(filename + "_accel.png")
-        plt.show()
-
-        x_m = [i[0] for i in mag]
-        y_m = [j[1] for j in mag]
-        z_m = [k[2] for k in mag]
-
-        plt.plot(x_m)
-        plt.plot(y_m)
-        plt.plot(z_m)
-        plt.savefig(filename + "_mag.png")
-        plt.show()
-
-        x_g = [i[0] for i in gyro]
-        y_g = [j[1] for j in gyro]
-        z_g = [k[2] for k in gyro]
-
-        plt.plot(x_g)
-        plt.plot(y_g)
-        plt.plot(z_g)
-        plt.savefig(filename + "_gyro.png")
-        plt.show()
+def kalman():
+    pass
 
 
-
-parse_data()
+def main():
+    rssi, accel, mag, gyro = parse_data()
